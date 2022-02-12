dd4b27f15c6e1461272466ba227102bf85a545ef
claudiomelendres <claudiomelendres@gmail.com>
2022-02-12 13:39:14 -0400

    first

---
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: [
+  ]
+})
+export class HeroeComponent implements OnInit {
+
+  constructor(private activateRouter: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activateRouter.params
+      .subscribe(console.log);
+
+
+  }
+
+}
